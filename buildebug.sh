dc74509791aa89229b9957448d1568c3ce435ea6
ferstar <zhangjianfei3@gmail.com>
2024-05-12 03:36:43 +0800

    update clang version

--- buildebug.sh
+++ buildebug.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 DIR=`readlink -f .`
 MAIN=`readlink -f ${DIR}/..`
-export CLANG_PATH=$MAIN/clang-r416183b/bin
+export CLANG_PATH=$MAIN/clang-r450784d/bin
 export PATH=${BINUTILS_PATH}:${CLANG_PATH}:${PATH}
 # Resources
 THREAD="-j$(nproc --all)"
 
-export CLANG_PATH=$MAIN/clang-r416183b/bin/
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=$MAIN/clang-r416183b/bin/aarch64-linux-gnu- CC=clang CXX=clang++
+export CROSS_COMPILE=$MAIN/clang-r450784d/bin/aarch64-linux-gnu- CC=clang CXX=clang++
 
 DEFCONFIG="gki_defconfig"
 
