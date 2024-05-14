825761b6c3ed82886b65270e40e9444cac009bea
ferstar <zhangjianfei3@gmail.com>
2024-05-15 06:47:37 +0800

    Revert "[ALPS07148096] GED : Add error handling for snprintf"
    This reverts commit 7410e73ac6d8ebecfd9d149587ebfc1542962820.

--- drivers/gpu/mediatek/ged/src/ged_log.c
+++ drivers/gpu/mediatek/ged/src/ged_log.c
@@ -1257,13 +1257,11 @@ void ged_log_perf_trace_batch_counter(char *name, long long count, int pid,
 	unsigned long frameID, u64 BQID, char *batch_str)
 {
 	char buf[256];
-	int cx;
 
 	if (ged_log_perf_trace_enable) {
-		cx = snprintf(buf, sizeof(buf), "C|%d|%s|%lld|%llu|%lu%s\n",
+		snprintf(buf, sizeof(buf), "C|%d|%s|%lld|%llu|%lu%s\n",
 		pid, name, count, (unsigned long long)BQID, frameID, batch_str);
-		if (cx >= 0 && cx < sizeof(buf))
-			tracing_mark_write(buf);
+		tracing_mark_write(buf);
 	}
 }
 EXPORT_SYMBOL(ged_log_perf_trace_batch_counter);
